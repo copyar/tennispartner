a0d51265d7f2413c9d5965e6eaa9b13e290a3e0d
Yarick Coppejans <yarick_coppejans@hotmail.com>
2022-08-21 12:34:21 +0200

    Added navigation.xml + ActionBar -> Toolbar

--- app/src/main/java/com/example/android/tennispartner/MainActivity.kt
+++ app/src/main/java/com/example/android/tennispartner/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.android.tennispartner
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolBar()
+    }
+
+    fun initToolBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_keyboard_arrow_left_24)
+
+        supportActionBar?.apply {
+            setDisplayShowHomeEnabled(false)
+            setHomeButtonEnabled(true)
+        }
     }
 }
